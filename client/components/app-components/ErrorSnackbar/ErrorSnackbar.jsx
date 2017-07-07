291768e2f2729b04039839a16eb3a689aecfb9a5
Viraj Trivedi <inf3cti0n95@gmail.com>
2017-07-07 23:39:31 +0530

    ErrorSnackbar Rebuilt, without using "refs"

--- client/components/app-components/ErrorSnackbar/ErrorSnackbar.jsx
+++ client/components/app-components/ErrorSnackbar/ErrorSnackbar.jsx
@@ -6,25 +6,34 @@ import { connect } from "react-redux";
 class ErrorSnackbar extends Component {
 	constructor(props){
 		super(props);
+		this.state = {error: props.error[props.error.length - 1]};
 	}
-	shouldComponentUpdate(nextProp){
-		return nextProp.error.length !== 0;
+	shouldComponentUpdate(nextProp, nextState){
+		return nextProp.error !== this.props.error || this.state.error !== nextState.error;
+	}
+	componentWillUpdate(nextProp, nextState){
+		this.setState({ error : nextProp.error[nextProp.error.length - 1] });
+		return  nextProp.error !== this.props.error || this.state.error !== nextState.error && this.state.error !== undefined;
 	}
 	componentDidUpdate(){
-		let nextProp = this.props;
-		let reason = nextProp.error[0].reason;
-		this.refs["err-snackbar"].classList.add("mdl-snackbar--active");
-		this.refs["err-text"].textContent = reason;
-		setTimeout(
+		if(this.state.error !== undefined)
+			setTimeout(
 			() => {
-				this.refs["err-snackbar"].classList.remove("mdl-snackbar--active");
-				this.props.dispatch({type: "REMOVE_ERROR", payload: nextProp.error[0]});
+				this.props.dispatch({type: "REMOVE_ERROR", payload: this.state.error});
+				this.setState({ error : this.props.error[this.props.error.length - 1] });
 			},2750);
+	}
+	isActive(){
+		return this.state.error === undefined ? "" : " mdl-snackbar--active";  
+	}
+
+	getReason(){
+		return this.state.error ? this.state.error.reason : "";
 	}
 	render() {
 		return (
-            <div ref="err-snackbar" id="err-snackbar" className="mdl-snackbar">
-                <div ref="err-text" className="mdl-snackbar__text"></div>
+            <div ref="err-snackbar" id="err-snackbar" className={"mdl-snackbar" + this.isActive()}>
+                <div ref="err-text" className="mdl-snackbar__text">{this.getReason()}</div>
                 <button className="mdl-snackbar__action" type="button"></button>
             </div>
 		);
