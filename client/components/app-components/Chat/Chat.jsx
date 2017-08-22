00bdc287c392cb01d2c9d77efab0ebb342422142
Viraj Trivedi <inf3cti0n95@gmail.com>
2017-08-22 23:02:08 +0530

    Chat component Implmented

--- client/components/app-components/Chat/Chat.jsx
+++ client/components/app-components/Chat/Chat.jsx
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { subscribeToRoom } from "@actions/roomActions";
 
 class Chat extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			lastestMessages: []
+		};
+	}
+	componentWillReceiveProps(nextProps){
+		this.setState({
+			...nextProps.location.state // Room Info in State
+		});
+		
+		if(nextProps.messages.fields)
+			this.setState({
+				...this.state,
+				lastestMessages : [...this.state.lastestMessages, nextProps.messages ? nextProps.messages.fields.args.map(field => field.msg) : null]
+
+			});
+	}
+	componentWillUpdate(nextProps, nextState){
+		// console.info("current room ==>",nextState.name,nextState);
+		
+	}
+	componentDidUpdate(prevProps, prevState){
+		if(prevState.rid !== this.state.rid){
+			this.props.dispatch({type:"CLOSE_SUB"}); //Unsuscribe to the Previous Channel
+			this.props.dispatch(subscribeToRoom(this.state.rid)); // Subscribe to the New Channel
+		}
+	}
 	render() {
 		return (
-            <div>
+			<div>
                 Chat {this.props.match.params.channelName || "No Room"}
-            </div>
+				{JSON.stringify(this.state.lastestMessages)}
+			</div>
 		);
 	}
 }
 
-export default Chat;
+export default connect(state => state)(Chat);
