59d66607d85dec2becf84848ff3723df889ee14c
Viraj Trivedi <inf3cti0n95@gmail.com>
2017-08-22 23:00:30 +0530

    Title In the Header

--- client/components/app-components/App/App.jsx
+++ client/components/app-components/App/App.jsx
@@ -4,6 +4,7 @@ import { Switch, Route, withRouter } from "react-router-dom";
 
 import Layout from "@utils/Layout";
 import Content from "@utils/Content";
+import Title from "@utils/Title";
 
 import Drawer from "@components/Drawer/Drawer";
 import Header from "@components/Header/Header";
@@ -26,9 +27,11 @@ class App extends Component {
     
 	render() {
 		return (
-            <Layout class="mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+			<Layout class="mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
 				<Drawer />
-				<Header />
+				<Header>
+					<Title>{this.props.location.state ? ("@"+this.props.location.state.name) : ""}</Title>
+				</Header>
 				<Content class="mdl-color--white">
 					<Switch>
 						<Route exact path="/" component={WelcomeScreen} />
